e43787323590e9600d8d4df0a9f6791486261abf
Kozlov Sergey <skozlov@sgfwest.com>
2023-03-30 23:20:20 +0600

    Animate Player character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
-
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -32,7 +32,13 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * movementSpeed * Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
