a7f1d22ef3b8a9d17c8ea68edf8b74fb06c3816e
Kozlov Sergey <izumael@bk.ru>
2023-05-23 14:38:30 +0600

    Import Unity 2d sprite package; implement world UI elements for ingredients on the plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -40,4 +40,10 @@ public class PlateKitchenObject : KitchenObject
             return true;
         }
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+
 }
